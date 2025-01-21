32ccf256e12e28959f3ba123979f1196be8401d1
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-01-20 21:08:55 -0300

    Mudanças visuais das views welcme e home.

--- sistema-culinario/resources/views/welcome.blade.php
+++ sistema-culinario/resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
 @section('title', 'Receitas Rápidas')
 @section('content')
     <div class="container">
-        <h1 style="text-align: center" class="mb-5">As mais acessadas.</h1>
+        <h1 style="text-align: center; font-size: 56px" class="mb-5 p-3 bg-white rounded">As mais acessadas.</h1>
         <div class="row m-2">
             @foreach ($receitas as $receita)
-                <div class="col-lg-4">
+                <div class="col-lg-4 bg-white transparente m-3 p-4 rounded">
                     <h2 style="text-align: center; color:cornflowerblue" class="fw-normal">{{ $receita->nome }}</h2>
                     <p style="text-align: justfy;"><strong>Ingredientes:</strong> {{ $receita->ingredientes }}.</p>
                     <p style="text-align: justfy;"><strong>Categoria:</strong> {{ $receita->categoria->nome }}.</p>
